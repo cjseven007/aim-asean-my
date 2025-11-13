706e0c81441afd6f369a5d5d217b3a7b8f810e58
CJ Seven <hperm9166@gmail.com>
2025-11-13 12:18:54 +0800

    feat: Added tailwind, primary colours and navbar cod

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home-page/home-page';
+import { CoursePage } from './course-page/course-page';
+import { AboutPage } from './about-page/about-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', component: HomePage, title: 'AIM ASEAN — Home' },
+    { path: 'courses', component: CoursePage, title: 'AIM ASEAN — Courses' },
+    { path: 'about', component: AboutPage, title: 'AIM ASEAN — About' },
+    { path: '**', redirectTo: '' },
+];
---
+++ src/app/course-page/course-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-page',
+  imports: [],
+  templateUrl: './course-page.html',
+  styleUrl: './course-page.css',
+})
+export class CoursePage {
+
+}
