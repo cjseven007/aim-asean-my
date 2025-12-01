37b0ee9444274f0b1c5ce873a9438dbc26610578
lesterkzj <lesterkzj0208@gmail.com>
2025-12-01 22:31:58 +0800

    designed card component, added grid to tools page and added some tool logos

--- src/components/detail-card/detail-card.ts
+++ src/components/detail-card/detail-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-detail-card',
+  selector: 'cmpnt-detail-card',
   imports: [],
   templateUrl: './detail-card.html',
   styleUrl: './detail-card.css',
 })
 export class DetailCard {
-
+  @Input() name!: string;           
+  @Input() description!: string;    
+  @Input() link!: string;           
+  @Input() pricing!: string;        
+  @Input() image!: string;
 }
